457f1169f859de4b6dff97c9cb831973e3d64eda
angelsumalini <70747259+angelsumalini@users.noreply.github.com>
2021-06-29 05:18:12 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,21 @@
+
+class WhileExample
+{
+    public static void main(String s[])
+    {
+        int n = 10;
+        int m = 50;
+
+        while( n > 0 && m > 0)
+        {
+            n--;
+
+            System.out.println("n = " + n);
+            m--;
+             System.out.println("m = " + m);
+
+
+
+        }
+    }
+}
